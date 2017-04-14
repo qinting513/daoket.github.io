a592c932aa4fa0539c4a5f8d4d60bb7901c3169e
daoket <daoket@163.com>
2017-04-15 00:31:58 +0800

    .

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# daoket.github.io
+## 欢迎访问我的github，下面是我的一些个人项目。
+1. [我的简历](https://daoket.github.io/resume/index.html)
+<center>
+<img src="https://daoket.github.io/resume/images/resume.png"/>
+<center/>
+
+2. [我的博客](https://daoket.github.io/blog/index.html)
+<center>
+<img src="https://daoket.github.io/resume/images/blog.png"/>
+<center/>
+
+3. [个人主页](https://daoket.github.io/home/index.html#/jx)
+<center>
+<img src="https://daoket.github.io/resume/images/home.png"/>
+<center/>
+
+4. [百度百家](https://daoket.github.io/baijia/index.html#/select)
+<center>
+<img src="https://daoket.github.io/resume/images/baijia.png"/>
+<center/>
---
+++ resume/old/js/json.js
@@ -0,0 +1,82 @@
+/**
+ * Created by Administrator on 2015/9/16.
+ */
+var Json = {
+    project:[
+        {
+            name:'微信商城',
+            date:'2016.6-2017.3',
+            detail:'为浙报集团制作的微信在线商城，兼容安卓、IOS，商城包含首页、精选、购物车、我的四大部分。',
+            image:['images/photo/1/1.png','images/photo/1/2.png','images/photo/1/3.png','images/photo/1/4.png'],
+            link:'打开微信 关注“微数据应用” 点击右下角的商城'
+        },
+        {
+            name:'微信商城后台',
+            date:'2016.6-2017.3',
+            detail:'为微信商城搭建的后台管理项目，主要分为商品、订单、统计分析、设置四大部分。',
+            image:['images/photo/2/1.png','images/photo/2/2.png','images/photo/2/3.png','images/photo/2/4.png'],
+            link:'//mall.mediadreamworks.net/ShopBack/Index/index'
+        },
+        {
+            name:'新媒体云平台',
+            date:'2016.5-2017.3',
+            detail:'浙报云平台项目，主要用于微信公众号的管理',
+            image:['images/photo/3/1.png','images/photo/3/2.png','images/photo/3/3.png','images/photo/3/4.png'],
+            link:'//test-op.mediadreamworks.net/'
+        },
+        {
+            name:'后台管理',
+            date:'2015.9',
+            detail:'浙报的另一个项目，主要是商城的商品和商家的管理',
+            image:['images/photo/4/1.png','images/photo/4/2.png','images/photo/4/3.png','images/photo/4/4.png'],
+            link:'//test-auth.mediadreamworks.net/'
+        },
+        {
+            name:'百度百家',
+            date:'2017.2',
+            detail:'这是我学习vue之后独立搭建的一个单页面应用，主要应用到到了vue-router等相关知识。',
+            image:['images/photo/5/1.png','images/photo/5/2.png','images/photo/5/3.png','images/photo/5/4.png'],
+            link:'//daoket.github.io/baijia/index.html#/jx'
+        },
+        {
+            name:'个人网站',
+            date:'2016.6-2017.3',
+            detail:'我的个人网站首页，包含了到我博客和github的链接',
+            image:['images/photo/6/1.png','images/photo/6/2.png','images/photo/6/3.png','images/photo/6/4.png'],
+            link:'//daoket.github.io/home/index.html'
+        }
+    ],
+    work: [
+        {
+            company:'随视传媒科技股份有限公司（杭州）',
+            job:'前端工程师',
+            date:'2016年5月-2017年3月',
+            detail:'负责pc端，手机端页面的制作。负责完成页面交互效果的制作。与后端配合完成数据交互。',
+            logo:'images/icons/header1.png'
+        }, {
+            company:'特奢汇科技有限公司',
+            job:'前端实习生',
+            date:'2015年7月-2017年4月',
+            detail:'公司项目中代码的优化，项目的升级，前端效果的实现等工作',
+            logo:'images/icons/header1.png'
+        }
+    ],
+    myInfo:{
+        name:'牛文通',
+        slogan:'两年前端经验',
+        edu:'本科 河南工程学院',
+        time:'2年',
+        sex:'1992年 男',
+        city:'杭州',
+        phone:'17342059140',
+        mail:'wt_niu@163.com',
+        work:'前端工程师，全职，杭州，12K',
+        work1:'前端工程师',
+        nature:'全职',
+        hopeCity:'杭州',
+        money:'12k',
+        head:'images/photo/head.jpg',
+        ps:'https://github.com/daoket'
+    }
+
+};

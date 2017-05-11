9fb52ffff4e5d70b109bfc1dc7eff9e890a2f838
daoket <daoket@163.com>
2017-05-11 22:59:09 +0800

    .

--- img/data.js
+++ img/data.js
@@ -1,4 +1,19 @@
 {
-  "name": "iwen",
-  "age": "18"
+  "data" : [{
+  "name": "张三",
+  "userNumber": "439000",
+  "prize": "1000"
+}, {
+  "name": "张三",
+  "userNumber": "439000",
+  "prize": "1000"
+}, {
+  "name": "张三",
+  "userNumber": "439000",
+  "prize": "1000"
+}, {
+  "name": "张三",
+  "userNumber": "439000",
+  "prize": "1000"
+}]
 }
